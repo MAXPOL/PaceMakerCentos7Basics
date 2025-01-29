a55ea6eedbfcf104c75519e81b2bf9befc5b0562
MAXPOL <maxrol32@gmail.com>
2025-01-29 20:41:55 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+PACEMAKER
+
+#Insatall on all servers
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS*
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS*
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS*
+yum install nano epel-release -y 
+yum install ntpdate pacemaker pcs resource-agents -y
+\cp /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+ntpdate ru.pool.ntp.org
+firewall-cmd --permanent --add-service=high-availability
+firewall-cmd --reload
+passwd hacluster
+systemctl enable pcsd
+systemctl start pcsd
