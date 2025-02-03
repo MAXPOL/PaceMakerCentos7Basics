51b8f1ba4a342e163459f9dac809f5887eb65672
MAXPOL <maxrol32@gmail.com>
2025-02-03 22:12:30 +0300

    Update InitialSetupAllServersCluster.sh

--- InitialSetupAllServersCluster.sh
+++ InitialSetupAllServersCluster.sh
@@ -7,6 +7,7 @@ sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS*
 sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS*
 yum install nano epel-release -y 
 yum install ntpdate pacemaker pcs resource-agents -y
+yum install -y corosync samba ctdb policycoreutils-python # IF NEED CREATE SAMBA CLUSTER
 \cp /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 ntpdate ru.pool.ntp.org
 firewall-cmd --permanent --add-service=high-availability
