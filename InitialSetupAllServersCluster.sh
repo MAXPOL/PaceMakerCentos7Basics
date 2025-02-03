270d7b50f547705752306318d6d7249d7c82e9f2
MAXPOL <maxrol32@gmail.com>
2025-02-03 22:15:12 +0300

    Update InitialSetupAllServersCluster.sh

--- InitialSetupAllServersCluster.sh
+++ InitialSetupAllServersCluster.sh
@@ -2,9 +2,6 @@
 
 #!/bin/bash
 
-sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS*
-sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS*
-sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS*
 yum install nano epel-release -y 
 yum install ntpdate pacemaker pcs resource-agents -y
 yum install -y corosync samba ctdb policycoreutils-python # IF NEED CREATE SAMBA CLUSTER
